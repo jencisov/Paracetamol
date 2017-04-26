9821b02d6e64beb68dc429a37363e504165d658e
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-25 21:00:18 -0500

    first commit

---
+++ Paracetamol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paracetamol
+//
+//  Created by Kuma on 4/25/17.
+//  Copyright © 2017 Kuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.red
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
