7f508098aa787a3442bc2bcf546f14005beb8704
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-27 18:45:39 -0500

    Added a ViewPicker

--- Paracetamol/ViewController.swift
+++ Paracetamol/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["Alabama","Alaska","Arkansas","California","Carolina","Columbia","Maine","New York","Washington"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = UIColor.red
-        
+        statePicker.dataSource=self
+        statePicker.delegate=self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    @IBAction func onStateButtonPressed(_ sender: Any) {
+        statePicker.isHidden=false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    
 }
-
