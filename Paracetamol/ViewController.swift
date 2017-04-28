3fd230dcb539ece1b074d77f931e2e03acba5222
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-27 21:11:05 -0500

    trying JetBrains' AppCode for programming

--- Paracetamol/ViewController.swift
+++ Paracetamol/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource,
-UIPickerViewDelegate {
-    
+        UIPickerViewDelegate {
+
     @IBOutlet weak var paracetamolImageView: UIImageView!
     @IBOutlet weak var paracetamolLabel: UILabel!
     @IBOutlet weak var paracetamolPriceLabel: UILabel!
@@ -24,84 +24,79 @@ UIPickerViewDelegate {
     @IBOutlet weak var stateLabel: UILabel!
     @IBOutlet weak var statePickerButton: UIButton!
     @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var countryLabel: UILabel!
-    @IBOutlet weak var countryTextField: UITextField!
-    @IBOutlet weak var zipCodeLabel: UILabel!
-    @IBOutlet weak var zipCodeTextField: UITextField!
-    
     @IBOutlet weak var buyNowButton: UIButton!
     @IBOutlet weak var successImageView: UIImageView!
-    
-    let states = ["Alabama","Alaska","Arkansas","California","Carolina",
-                  "Columbia","Maine","New York","Washington"]
-    
+
+    let states = ["Alabama", "Alaska", "Arkansas", "California", "Carolina",
+                  "Columbia", "Maine", "New York", "Washington"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        statePicker.dataSource=self
-        statePicker.delegate=self
+        statePicker.dataSource = self
+        statePicker.delegate = self
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        successImageView.isUserInteractionEnabled = true
+        successImageView.addGestureRecognizer(tapGestureRecognizer)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     @IBAction func onStateButtonPressed(_ sender: Any) {
         showStatePicker(isShowing: true)
     }
-    
+
     @IBAction func onBuyButtonPressed(_ sender: Any) {
-        buy()
+        buy(isBuying: true)
     }
-    
+
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        buy(isBuying: false)
+    }
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int {
         return states.count
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int,
                     forComponent component: Int) -> String? {
         return states[row]
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
                     inComponent component: Int) {
         statePickerButton.setTitle(states[row], for: UIControlState.normal)
         showStatePicker(isShowing: false)
     }
-    
-    func showStatePicker(isShowing : Bool){
+
+    func showStatePicker(isShowing: Bool) {
         statePicker.isHidden = !isShowing
-        countryLabel.isHidden = isShowing
-        countryTextField.isHidden = isShowing
-        zipCodeLabel.isHidden = isShowing
-        zipCodeTextField.isHidden = isShowing
+        buyNowButton.isHidden = isShowing
     }
-    
-    func buy(){
-        paracetamolImageView.isHidden=true
-        paracetamolLabel.isHidden=true
-        paracetamolPriceLabel.isHidden=true
-        divider.isHidden=true
-        fullNameLabel.isHidden=true
-        fullNameTextField.isHidden=true
-        addressLabel.isHidden=true
-        addressTextField.isHidden=true
-        cityLabel.isHidden=true
-        cityTextField.isHidden=true
-        stateLabel.isHidden=true
-        statePickerButton.isHidden=true
-        statePicker.isHidden=true
-        countryLabel.isHidden=true
-        countryTextField.isHidden=true
-        zipCodeLabel.isHidden=true
-        zipCodeTextField.isHidden=true
-        
-        buyNowButton.isHidden=true
-        successImageView.isHidden=false
+
+    func buy(isBuying: Bool) {
+        paracetamolImageView.isHidden = isBuying
+        paracetamolLabel.isHidden = isBuying
+        paracetamolPriceLabel.isHidden = isBuying
+        divider.isHidden = isBuying
+        fullNameLabel.isHidden = isBuying
+        fullNameTextField.isHidden = isBuying
+        addressLabel.isHidden = isBuying
+        addressTextField.isHidden = isBuying
+        cityLabel.isHidden = isBuying
+        cityTextField.isHidden = isBuying
+        stateLabel.isHidden = isBuying
+        statePickerButton.isHidden = isBuying
+
+        buyNowButton.isHidden = isBuying
+        successImageView.isHidden = !isBuying
     }
-    
+
 }
